64cc9d19e4fc79a3c5a43985c799be963a8bef91
rogergonzalez99 <roger.gh77@gmail.com>
2021-02-25 13:13:21 +0100

    tercera versio

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,18 @@
+
+/* custom element*/
+
 class MyComponent extends HTMLElement {
     connectedCallback() {
-      this.innerHTML = `<h1>Hello world</h1>`;
+        this.innerHTML = `<h1>Hello world</h1> 
+        <p> Aqui estem </p>
+        <button> Clica aqui sobre </button>`;
     }
   }
       
   customElements.define('my-component', MyComponent); /*aixo defineix el component per poder-lo utilitzar al index.html*/
+
+
+  /*shadow DOM*/
 
   const shadowRoot = document.getElementById('example').attachShadow({ mode: 'open' });
   shadowRoot.innerHTML = `<style>
@@ -14,3 +22,4 @@ class MyComponent extends HTMLElement {
   }
   </style>
   <button id="button"><slot></slot> tomato</button>`;
+  
